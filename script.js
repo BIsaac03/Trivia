f26999944e7590be399f36b5c4fea7c49a71abfe
Benjamin Isaac <benisaac03@gmail.com>
2026-05-21 18:40:23 -0400

    Disregard all submissions after the first

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 console.log("hello world");
 
 
-
+let hasSubmitted = false;
 const submitBtn = document.querySelector(`.guess .submit`);
 submitBtn.addEventListener("click", () => {
-    const userGuess = document.querySelector(`.guess .guess`);
-    console.log(userGuess.value);
-    userGuess.placeholder = "Submitted!";
-    userGuess.value = "";
+    if (!hasSubmitted){
+        const userGuess = document.querySelector(`.guess .guess`);
+        hasSubmitted = true;
+        console.log(userGuess.value);
+        userGuess.placeholder = "Submitted!";
+        userGuess.value = "";
+    }
 })

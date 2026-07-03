d45e916763533e4f0d2a2320f97508ae02c9c59e
Benjamin Isaac <benisaac03@gmail.com>
2026-07-03 15:19:14 -0400

    Temporarily limit functionality to test connection

--- script.js
+++ script.js
@@ -39,19 +39,3 @@ io.use((socket, next) => {
 io.on("connection", (socket) => {
     console.log("connected");
 })
-
-let hasSubmitted = false;
-const submitBtn = document.querySelector(`.guess .submit`);
-submitBtn.addEventListener("click", () => {
-    if (!hasSubmitted){
-        const userGuess = document.querySelector(`.guess .guess`);
-        hasSubmitted = true;
-        console.log(userGuess.value);
-        userGuess.placeholder = "Submitted!";
-        userGuess.disabled = true;
-        userGuess.value = "";
-
-        const answers = document.getElementsByClassName("answers")[0];
-        answers.style.display = "grid";
-    }
-})
--- static/client.js
+++ static/client.js
@@ -3,3 +3,21 @@ const socket = io.connect("https://trivia-k294.onrender.com/");
 socket.on("connect", () => {  
     console.log("connected");
 });
+
+/*
+let hasSubmitted = false;
+const submitBtn = document.querySelector(`.guess .submit`);
+submitBtn.addEventListener("click", () => {
+    if (!hasSubmitted){
+        const userGuess = document.querySelector(`.guess .guess`);
+        hasSubmitted = true;
+        console.log(userGuess.value);
+        userGuess.placeholder = "Submitted!";
+        userGuess.disabled = true;
+        userGuess.value = "";
+
+        const answers = document.getElementsByClassName("answers")[0];
+        answers.style.display = "grid";
+    }
+})
+*/
